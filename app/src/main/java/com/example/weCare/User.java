1769a9a35b4d148f9cc287d465b6572eda7468d2
Abdullah A. Yassin <44100412+vintx64@users.noreply.github.com>
2020-07-27 17:58:32 +0200

    first commit

---
+++ app/src/main/java/com/example/weCare/User.java
@@ -0,0 +1,12 @@
+package com.example.weCare;
+class User {
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    private String status;
+}
